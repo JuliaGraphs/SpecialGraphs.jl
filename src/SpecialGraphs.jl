d9283aa80fccca01192f283b04840f839ddde441
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-06-30 23:09:28 +0200

    Files generated by PkgTemplates

---
+++ src/SpecialGraphs.jl
@@ -0,0 +1,5 @@
+module SpecialGraphs
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SpecialGraphs
+using Test
+
+@testset "SpecialGraphs.jl" begin
+    # Write your own tests here.
+end
