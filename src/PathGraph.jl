5d9918ee7e837a0b983bc08f71057364004c6c7b
Simon Schoelly <simon.schoelly@locatee.ch>
2019-10-12 14:45:52 +0200

    Add overrides for PathGraph

--- src/PathGraph.jl
+++ src/PathGraph.jl
@@ -45,3 +45,42 @@ function LightGraphs.has_edge(g::PathGraph, v1, v2)
     end
     return abs(v1-v2) == 1
 end
+
+# =======================================================
+#         overrides
+# =======================================================
+
+# we use this check so that we have the same convention as in LightGraphs
+LG.is_connected(g::PathGraph) = nv(g) > 0
+
+LG.connected_components(g::PathGraph) = [vertices(g)]
+
+# has_self_loops is defined in terms of this
+LG.num_self_loops(::PathGraph) = 0
+
+LG.is_bipartite(g::PathGraph) = true
+
+function LG.squash(g::PathGraph)
+    nvg = nv(g)
+    for T ∈ (UInt8, UInt16, UInt32, UInt64)
+        nv(g) < typemax(T) && return PathGraph(T(nvg))
+    end
+end
+
+# ---- degree -----------------------------------
+
+function LG.Δ(g::PathGraph)
+    nvg = nv(g)
+    return typemin(Int) * (nvg == 0) + (nvg >= 2) + (nvg >= 3)
+end
+
+LG.Δout(g::PathGraph) = Δ(g)
+LG.Δin(g::PathGraph) = Δ(g)
+
+function LG.δ(g::PathGraph)
+    nvg = nv(g)
+    return ifelse(nvg == 0, typemax(Int), ifelse(nvg == 1, 0, 1))
+end
+
+LG.δout(g::PathGraph) = δ(g)
+LG.δin(g::PathGraph) = δ(g)
